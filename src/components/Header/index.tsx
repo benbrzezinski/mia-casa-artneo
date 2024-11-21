7613d75bdcd8831374ee0ca8b7245104084ff8e9
Beniamin Brzeziński <benek9311@wp.pl>
2024-11-21 12:14:46 +0100

    Praca nad stroną główną, dodano header, kolejne zmienne i style globalne

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,53 @@
+import scss from "./Header.module.scss";
+
+export default function Header() {
+  return (
+    <header className="uk-background-secondary uk-flex uk-flex-between uk-flex-middle uk-padding">
+      <a href="#">
+        <img
+          src="/assets/images/logo-white-215.png"
+          alt="logo"
+          sizes="215px"
+          srcSet="/assets/images/logo-white-215.png 215w,
+                  /assets/images/logo-white-430.png 430w,
+                  /assets/images/logo-white-645.png 645w,
+                  /assets/images/logo-white-860.png 860w"
+        />
+      </a>
+      <nav>
+        <ul className={`uk-flex ${scss.list}`}>
+          <li>
+            <a href="#" className={`uk-link-reset ${scss.link}`}>
+              Home
+            </a>
+          </li>
+          <li>
+            <a href="#" className={`uk-link-reset ${scss.link}`}>
+              O nas
+            </a>
+          </li>
+          <li>
+            <a href="#" className={`uk-link-reset ${scss.link}`}>
+              Oferta
+            </a>
+          </li>
+          <li>
+            <a href="#" className={`uk-link-reset ${scss.link}`}>
+              Opinie klientów
+            </a>
+          </li>
+          <li>
+            <a href="#" className={`uk-link-reset ${scss.link}`}>
+              Blog
+            </a>
+          </li>
+          <li>
+            <a href="#" className={`uk-link-reset ${scss.link}`}>
+              Kontakt
+            </a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ src/components/Hero/index.tsx
@@ -0,0 +1,9 @@
+import Header from "../Header";
+
+export default function Hero() {
+  return (
+    <div>
+      <Header />
+    </div>
+  );
+}
