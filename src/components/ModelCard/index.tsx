653b197b5f287c12efb1db3f8a30c045ac6218f6
Beniamin Brzeziński <benek9311@wp.pl>
2024-11-27 17:03:10 +0100

    Ukończono listę modeli, dokończono komponent ModelCard, dodano przycisk poniżej listy

--- src/components/ModelCard/index.tsx
+++ src/components/ModelCard/index.tsx
@@ -9,25 +9,58 @@ const STEPS = [
   { title: "Etap V: ", text: "Pomoc w organizacji zarządzania wynajmem" },
 ] as const;
 
-export default function ModelCard() {
+interface ModelCardProps {
+  model: string | number;
+  title: string;
+  subtitle: string;
+  region: string;
+  regionImg: string;
+  notIncludedSteps: number[];
+}
+
+export default function ModelCard({
+  model,
+  title,
+  subtitle,
+  region,
+  regionImg,
+  notIncludedSteps,
+}: ModelCardProps) {
   return (
-    <div>
+    <li className={scss.card}>
       <div className={`uk-position-relative ${scss.modelBox}`}>
-        <p className={scss.modelText}>model 01</p>
-        <h3 className={scss.title}>Kompleksowe wsparcie</h3>
-        <p className={scss.subtitle}>Wyszukiwanie + Weryfikacja + Zakup</p>
+        <p className={scss.modelText}>model {model}</p>
+        <h3 className={scss.title}>{title}</h3>
+        <p className={scss.subtitle}>{subtitle}</p>
       </div>
       <div
         className={`uk-flex uk-flex-between uk-flex-middle ${scss.regionBox}`}
       >
         <p className={scss.regionText}>
-          <b>Region: </b>całe Włochy
+          <b>Region: </b>
+          {region}
         </p>
-        <img
-          loading="lazy"
-          src="/assets/svgs/italy.svg"
-          alt="mapa Włoch przedstawiająca obszar współpracy"
-        />
+        {region === "wzgórza Prosecco" ? (
+          <div className="uk-position-relative">
+            <img
+              loading="lazy"
+              src={regionImg}
+              alt="mapa Włoch przedstawiająca obszar współpracy"
+            />
+            <img
+              className={`uk-position-absolute ${scss.proseccoImg}`}
+              loading="lazy"
+              src="/assets/svgs/prosecco-hills.svg"
+              alt="wzgórza Prosecco na mapie Włoch"
+            />
+          </div>
+        ) : (
+          <img
+            loading="lazy"
+            src={regionImg}
+            alt="mapa Włoch przedstawiająca obszar współpracy"
+          />
+        )}
       </div>
       <div className={scss.stepsBox}>
         <ul className={`uk-flex uk-flex-column ${scss.stepsList}`}>
@@ -36,10 +69,17 @@ export default function ModelCard() {
               <img
                 loading="lazy"
                 src="/assets/svgs/checkmark.svg"
-                alt="obejmuje"
+                alt={notIncludedSteps.includes(i) ? "nie obejmuje" : "obejmuje"}
               />
               <div>
-                <p className={scss.stepsText}>
+                <p
+                  className={scss.stepsText}
+                  style={
+                    notIncludedSteps.includes(i)
+                      ? { textDecoration: "line-through" }
+                      : undefined
+                  }
+                >
                   <b>{title}</b>
                   {text}
                 </p>
@@ -48,6 +88,6 @@ export default function ModelCard() {
           ))}
         </ul>
       </div>
-    </div>
+    </li>
   );
 }
--- src/components/ModelsOverview/index.tsx
+++ src/components/ModelsOverview/index.tsx
@@ -1,14 +1,54 @@
 import ModelCard from "../ModelCard";
 import scss from "./ModelsOverview.module.scss";
 
+const CARDS_DETAILS = [
+  {
+    model: "01",
+    title: "Kompleksowe wsparcie",
+    subtitle: "Wyszukiwanie + Weryfikacja + Zakup",
+    region: "całe Włochy",
+    regionImg: "/assets/svgs/italy.svg",
+    notIncludedSteps: [],
+  },
+  {
+    model: "02",
+    title: "Wsparcie przy zakupie",
+    subtitle: "Weryfikacja + Zakup",
+    region: "całe Włochy",
+    regionImg: "/assets/svgs/italy.svg",
+    notIncludedSteps: [1],
+  },
+  {
+    model: "03",
+    title: "Częściowe wsparcie",
+    subtitle: "Zakup",
+    region: "wzgórza Prosecco",
+    regionImg: "/assets/svgs/italy-muted.svg",
+    notIncludedSteps: [1, 2],
+  },
+];
+
 export default function ModelsOverview() {
   return (
-    <div>
-      <ul>
-        <li className={scss.item}>
-          <ModelCard />
-        </li>
+    <div className={`uk-flex uk-flex-column uk-flex-middle ${scss.box}`}>
+      <ul className={`uk-flex uk-flex-center uk-flex-wrap ${scss.list}`}>
+        {CARDS_DETAILS.map(
+          ({ model, title, subtitle, region, regionImg, notIncludedSteps }) => (
+            <ModelCard
+              key={model}
+              model={model}
+              title={title}
+              subtitle={subtitle}
+              region={region}
+              regionImg={regionImg}
+              notIncludedSteps={notIncludedSteps}
+            />
+          )
+        )}
       </ul>
+      <button type="button" className={scss.btn}>
+        DOWIEDZ SIĘ WIĘCEJ
+      </button>
     </div>
   );
 }
