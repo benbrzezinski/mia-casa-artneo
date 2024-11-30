c2a8217741e618623a55a94f4fbf0d8f6e0e090b
Beniamin Brzeziński <benek9311@wp.pl>
2024-11-30 19:34:49 +0100

    Dodano do strony Offer: LeftBorder, RightBorder i Footer - w którym dostosowano ItalianSideLabel

--- src/containers/Footer/index.tsx
+++ src/containers/Footer/index.tsx
@@ -5,14 +5,20 @@ import FooterNav from "../../components/FooterNav";
 import ItalianSideLabel from "../../components/ItalianSideLabel";
 import scss from "./Footer.module.scss";
 
-export default function Footer() {
+interface FooterProps {
+  sideLabel?: boolean;
+}
+
+export default function Footer({ sideLabel = true }: FooterProps) {
   return (
     <footer className={`uk-position-relative ${scss.footer}`}>
-      <ItalianSideLabel
-        labelColor="#7a7a7a"
-        middleChildColor="#d3d3d3"
-        style={{ position: "absolute", top: 30, left: 26 }}
-      />
+      {sideLabel && (
+        <ItalianSideLabel
+          labelColor="#7a7a7a"
+          middleChildColor="#d3d3d3"
+          style={{ position: "absolute", top: 30, left: 26 }}
+        />
+      )}
       <div className={scss.bg}>
         <div className={`uk-container uk-container-large ${scss.wrapper}`}>
           <FooterLogo />
--- src/pages/Offer.tsx
+++ src/pages/Offer.tsx
@@ -1,6 +1,9 @@
 import ContentHero from "../containers/ContentHero";
 import CollaborationModels from "../containers/CollaborationModels";
 import WorkflowSteps from "../containers/WorkflowSteps";
+import LeftBorder from "../components/LeftBorder";
+import RightBorder from "../components/RightBorder";
+import Footer from "../containers/Footer";
 
 export default function Offer() {
   return (
@@ -9,7 +12,10 @@ export default function Offer() {
       <main>
         <CollaborationModels elementBelow="text" />
         <WorkflowSteps />
+        <LeftBorder height="100%" />
+        <RightBorder height="98%" />
       </main>
+      <Footer sideLabel={false} />
     </>
   );
 }
